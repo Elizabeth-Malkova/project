f89548a38c4d6b397630dca4f3ae595ac8474513
E <elizabeth.malkova@gmail.com>
2021-01-08 20:38:29 +0200

    first lessons

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 "use strict";
-const res = confirm("jhjkg")
- console.log(res)
+//1
+let numbersOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+console.log(numbersOfFilms)
+
+//2
+
+let personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+//3
+let a = prompt("Один из последних просмотренных фильмов?", " "),
+    b = prompt("На сколько оцените его?", " "),
+    c = prompt("Один из последних просмотренных фильмов?", " "),
+    d = prompt("На сколько оцените его?", " ");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
